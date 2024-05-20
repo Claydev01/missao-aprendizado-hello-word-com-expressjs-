3374eb44e10f722d3a601321578c86e77aad2913
Clay dev 01 <123648066+Claydev01@users.noreply.github.com>
2024-05-20 14:30:10 +0000

    Endpoint '/oi' adicionado  e criação do script 'start'

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+// Endpoint /oi -> Olá, mundo!
+
+app.get('/oi', function (req, res){
+  res.send('Olá,Mundo')
+})
 app.listen(3000)
